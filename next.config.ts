26d60c7bff88d4bb1d3bef3d7600e36f91978675
Akhilesh <akhilesh.talekar21@gmail.com>
2025-05-14 18:52:09 +0530

    Configure TypeScript and ESLint to ignore build errors during development

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import { hostname } from "os";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript:{
+    ignoreBuildErrors: true,
+  },
+  
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+
   images: {
     remotePatterns: [{
       hostname: "logo.clearbit.com",
